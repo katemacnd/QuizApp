58c3c924e412fe37b6afa06803cc4aa2dd3312f5
Kate Engstrom <katemacnd@gmail.com>
2019-09-24 15:17:36 -0500

    adding js

---
+++ index.js
@@ -0,0 +1,32 @@
+//quiz questions, options, correct responses
+
+//functions related to START button
+    //clicking START
+
+//functions related to quiz questions
+    //hide START button
+    //unhide questions, answer options, submit button, and stats
+    //clicking an answer
+    //clicking submit
+    //determine if answer correct/incorrect
+
+//functions related to quiz results
+    //hide questions, answer options, submit button,
+    //display image/result (based on correct/incorrect answer), correct answer, NEXT button
+    //updating score
+    //updating question count
+    //clicking NEXT button
+
+// functions related to quiz questions (con't)
+    //display stats
+    //change questions/answers
+
+//functions related to quiz end
+    //display final score
+    //clicking RESTART
+
+// click listeners
+    //START
+    //SUBMIT
+    //NEXT
+    //RESTART
